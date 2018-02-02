7d7c09b6753a86e15e3dafed585cd586d01a4baa
Alex McArther <acmcarther@google.com>
2018-02-01 19:26:37 -0800

    Debug osx

--- examples/hello_runfiles/src/lib.rs
+++ examples/hello_runfiles/src/lib.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 /// Returns the .runfiles directory for the currently executing binary.
 pub fn get_runfiles_dir() -> io::Result<PathBuf> {
     let mut path = std::env::current_exe()?;
+    println!("current_exe: {:?}", path);
 
     let mut name = path.file_name().unwrap().to_owned();
     name.push(".runfiles");
@@ -26,6 +27,11 @@ mod test {
     #[test]
     fn test_can_read_data_from_runfiles() {
         let runfiles = get_runfiles_dir().unwrap();
+        println!("supposed runfiles dir: {:?}", runfiles);
+
+        for entry in std::fs::read_dir(&runfiles) {
+          println!("entry in 'runfiles' {:?}", entry);
+        }
 
         let mut f = File::open(runfiles.join("examples/hello_runfiles/data/sample.txt")).unwrap();
         let mut buffer = String::new();
