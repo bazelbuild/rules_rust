70d54acc9c6c5d28ef1d6564e1ce6cf173ba281a
Alex McArther <acmcarther@google.com>
2018-02-13 13:15:26 -0800

    Remove changes to example

--- examples/hello_runfiles/src/lib.rs
+++ examples/hello_runfiles/src/lib.rs
@@ -6,16 +6,17 @@ pub fn get_runfiles_dir() -> io::Result<PathBuf> {
     let mut path = std::env::current_exe()?;
 
     if cfg!(target_os = "macos") {
-        path.pop();
+      path.pop();
     } else {
-        let mut name = path.file_name().unwrap().to_owned();
-        name.push(".runfiles");
-        path.pop();
-        path.push(name);
+      let mut name = path.file_name().unwrap().to_owned();
+      name.push(".runfiles");
+      path.pop();
+      path.push(name);
     }
 
     Ok(path)
 }
+
 
 #[cfg(test)]
 mod test {
@@ -30,9 +31,9 @@ mod test {
         let runfiles = get_runfiles_dir().unwrap();
 
         let mut f = if cfg!(target_os = "macos") {
-            File::open(runfiles.join("data/sample.txt")).unwrap()
+          File::open(runfiles.join("data/sample.txt")).unwrap()
         } else {
-            File::open(runfiles.join("examples/hello_runfiles/data/sample.txt")).unwrap()
+          File::open(runfiles.join("examples/hello_runfiles/data/sample.txt")).unwrap()
         };
         let mut buffer = String::new();
 
