6873ab8550e402f7993f28fa290f562db854dd01
Marco Farrugia <afmarco@gmail.com>
2018-10-19 20:18:36 -0400

    Spice up runfiles example.

--- examples/hello_runfiles/hello_runfiles.rs
+++ examples/hello_runfiles/hello_runfiles.rs
@@ -8,10 +8,11 @@ use runfiles::Runfiles;
 fn main() {
     let r = Runfiles::create().unwrap();
 
-    let mut f = File::open(r.rlocation("examples/hello_runfiles/data/sample.txt")).unwrap();
+    let mut f = File::open(r.rlocation("examples/hello_runfiles/hello_runfiles.rs")).unwrap();
 
     let mut buffer = String::new();
     f.read_to_string(&mut buffer).unwrap();
 
-    println!("{}", buffer);
+    assert_eq!(buffer.len(), 427);
+    println!("This program's source is:\n```\n{}\n```", buffer);
 }
