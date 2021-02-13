080055bfa851cbae9c39ece565990d319c247fde
Andre Brisco <andre.brisco@gmail.com>
2021-02-13 11:41:42 -0800

    Fixed expanded locations in test launcher

--- test/test_env/tests/run.rs
+++ test/test_env/tests/run.rs
@@ -6,5 +6,18 @@ fn run() {
 
     // Test the `env` attribute of `rust_test` at run time
     assert_eq!(std::env::var("FERRIS_SAYS").unwrap(), "Hello fellow Rustaceans!");
-    assert_eq!(std::env::var("HELLO_WORLD_BIN").unwrap(), "test/test_env/hello-world");
+
+    // Test the behavior of `rootpath` and that a binary can be found relative to current_dir
+    let hello_world_bin = std::path::PathBuf::from(std::env::var("HELLO_WORLD_BIN_ROOTPATH").unwrap());
+    assert_eq!(
+        hello_world_bin.display().to_string(), 
+        std::path::Path::new("test/test_env/hello-world").display().to_string(),
+    );
+    assert!(!hello_world_bin.is_absolute());
+    assert!(hello_world_bin.exists());
+
+    // Ensure `execpath` expanded variables map to real files and have absolute paths
+    let hello_world_bin = std::path::PathBuf::from(std::env::var("HELLO_WORLD_SRC_EXECPATH").unwrap());
+    assert!(hello_world_bin.is_absolute());
+    assert!(hello_world_bin.exists());
 }
