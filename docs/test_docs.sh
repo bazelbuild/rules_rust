d691eacc1bc64cca0fac9a8abc3be7b35d52037a
Andre Brisco <andre.brisco@gmail.com>
2021-03-31 10:19:08 -0700

    Added test script for ensuring docs are up to date.

--- docs/test_docs.sh
+++ docs/test_docs.sh
@@ -15,8 +15,12 @@ pushd "${DOCS_WORKSPACE}" &> /dev/null
 bazel clean \
 && bazel build //... \
 && cp bazel-bin/*.md . \
-&& chmod 0644 *.md \
-&& git add *.md \
-&& git commit -m "Regenerate documentation"
-popd &> /dev/null
+&& chmod 0644 *.md
 
+if [ -n "$(git status --porcelain)" ]; then 
+    git status
+    echo '/docs is out of date. Please run `./docs/update_docs.sh` from the root of rules_rust and push the results' >&2
+    exit 1
+fi
+
+popd &> /dev/null
