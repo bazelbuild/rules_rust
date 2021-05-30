0e95409ff4b9f5b54c9bc9e12a7f2b15310d10fe
Marcel Hlopko <hlopko@google.com>
2021-05-30 07:22:19 +0200

    Use runfiles instead

--- test/rust_analyzer/rust_project_json_test.rs
+++ test/rust_analyzer/rust_project_json_test.rs
@@ -1,11 +1,18 @@
-use std::env;
+use runfiles::Runfiles;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let rust_project_path = args.get(1).expect("expected rust-project.json path as first argument.");
-    let content = std::fs::read_to_string(rust_project_path).expect(&format!("couldn't open {}", rust_project_path));
+    let r = Runfiles::create().unwrap();
+    let rust_project_path = r.rlocation("rules_rust/test/rust_analyzer/rust-project.json");
 
-    for dep in &["lib_dep", "extra_test_dep", "proc_macro_dep", "extra_proc_macro_dep"] {
+    let content = std::fs::read_to_string(&rust_project_path)
+        .expect(&format!("couldn't open {:?}", &rust_project_path));
+
+    for dep in &[
+        "lib_dep",
+        "extra_test_dep",
+        "proc_macro_dep",
+        "extra_proc_macro_dep",
+    ] {
         if !content.contains(dep) {
             panic!("expected rust-project.json to contain {}.", dep);
         }
