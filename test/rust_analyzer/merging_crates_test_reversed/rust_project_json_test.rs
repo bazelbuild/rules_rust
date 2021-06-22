d82fe4a6b1fbf83b537d9634b32214f363c1f1b9
Marcel Hlopko <hlopko@google.com>
2021-06-22 09:25:58 +0200

    Rustfmt

--- test/rust_analyzer/merging_crates_test_reversed/rust_project_json_test.rs
+++ test/rust_analyzer/merging_crates_test_reversed/rust_project_json_test.rs
@@ -7,7 +7,9 @@ mod tests {
     #[test]
     fn test_deps_of_crate_and_its_test_are_merged() {
         let r = Runfiles::create().unwrap();
-        let rust_project_path = r.rlocation("rules_rust/test/rust_analyzer/merging_crates_test_reversed/rust-project.json");
+        let rust_project_path = r.rlocation(
+            "rules_rust/test/rust_analyzer/merging_crates_test_reversed/rust-project.json",
+        );
 
         let content = std::fs::read_to_string(&rust_project_path)
             .expect(&format!("couldn't open {:?}", &rust_project_path));
