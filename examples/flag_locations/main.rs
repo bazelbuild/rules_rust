c92bbb88a570923f0ecc67bf0d418ae65a632aca
RS <sayrer@gmail.com>
2021-06-28 09:45:02 -0700

    Rewrite to satisfy clippy.

--- examples/flag_locations/main.rs
+++ examples/flag_locations/main.rs
@@ -2,5 +2,9 @@
 #[test]
 fn test() {
     // we should able to read rustc args from a generated file
-    assert!(cfg!(test_flag));
+    if cfg!(test_flag) {
+        return;
+    }
+
+    unreachable!();
 }
