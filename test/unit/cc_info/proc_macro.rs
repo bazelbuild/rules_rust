589e78e0781748beec42efa7947ac054c6e97bee
UebelAndre <github@uebelandre.com>
2021-06-30 06:50:05 -0700

    Better fix for compile warning

--- test/unit/cc_info/proc_macro.rs
+++ test/unit/cc_info/proc_macro.rs
@@ -1,10 +1,11 @@
 use proc_macro::TokenStream;
 
 extern "C" {
-    #[allow(dead_code)]
     fn native_dep() -> isize;
 }
+
 #[proc_macro_derive(UsingNativeDep)]
-pub fn use_native_dep(_input: TokenStream) -> TokenStream {
-    panic!("done")
+pub fn use_native_dep(input: TokenStream) -> TokenStream {
+    println!("{}", unsafe { native_dep() });
+    input
 }
