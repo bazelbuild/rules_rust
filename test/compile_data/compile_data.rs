4cd549899ffa9ad867cd3fbdc977861d94a817cf
UebelAndre <github@uebelandre.com>
2021-07-15 05:15:43 -0700

    Added a wrapper test module

--- test/compile_data/compile_data.rs
+++ test/compile_data/compile_data.rs
@@ -1,9 +1,14 @@
 /// Data loaded from compile data
 pub const COMPILE_DATA: &str = include_str!("compile_data.txt");
 
-/// A test that is expected to be compiled from a target that does not
-/// directly populate the `compile_data` attribute
-#[test]
-fn test_compile_data_contents() {
-    assert_eq!(COMPILE_DATA, "compile data contents\n");
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    /// A test that is expected to be compiled from a target that does not
+    /// directly populate the `compile_data` attribute
+    #[test]
+    fn test_compile_data_contents() {
+        assert_eq!(COMPILE_DATA, "compile data contents\n");
+    }
 }
