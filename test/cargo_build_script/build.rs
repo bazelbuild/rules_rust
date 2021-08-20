d51da84e3421030cd26e9903a7352e88bd7226af
David Marcin <david@metawork.com>
2021-08-20 15:41:31 -0700

    Fix lint

--- test/cargo_build_script/build.rs
+++ test/cargo_build_script/build.rs
@@ -1,4 +1,7 @@
 fn main() {
     // Pass the TOOL_PATH along to the rust_test so we can assert on it.
-    println!("cargo:rustc-env=TOOL_PATH={}", std::env::var("TOOL").unwrap());
+    println!(
+        "cargo:rustc-env=TOOL_PATH={}",
+        std::env::var("TOOL").unwrap()
+    );
 }
