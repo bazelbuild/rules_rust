a9ae547800a9d716ea9b0f1371070a2adf358c39
Robert Sayre <sayrer@gmail.com>
2021-09-22 12:03:57 -0700

    Add some env printing, and make the test fail on purpose.

--- test/out_dir_in_tests/src/lib.rs
+++ test/out_dir_in_tests/src/lib.rs
@@ -6,8 +6,11 @@ mod tests {
 
     #[test]
     fn can_find_the_out_dir_file() {
+        for (key, value) in env::vars() {
+            println!("{}: {}", key, value);
+        }
         let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
         let contents = fs::read_to_string(out_path.join("test_content.txt")).unwrap();
-        assert_eq!("Test content", contents);
+        assert_eq!("FAIL ON PURPOSE", contents);
     }
 }
