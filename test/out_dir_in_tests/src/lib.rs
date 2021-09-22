6e4d02368d5a35f86d64ec8d95078a24ac384b77
Robert Sayre <sayrer@gmail.com>
2021-09-22 11:53:21 -0700

    Add test case for OUT_DIR in tests.

---
+++ test/out_dir_in_tests/src/lib.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use std::fs;
+    use std::path::PathBuf;
+
+    #[test]
+    fn can_find_the_out_dir_file() {
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        let contents = fs::read_to_string(out_path.join("test_content.txt")).unwrap();
+        assert_eq!("Test content", contents);
+    }
+}
