136fabae58c41276327954875d6fec5bdbdad5ec
cfredric <cfredric@chromium.org>
2022-01-28 11:24:28 -0500

    Implement transition for disabling renaming of first-party crates.

---
+++ examples/import/mod2.rs
@@ -0,0 +1,25 @@
+
+import::import! {
+    "//test/import:mod1";
+}
+
+pub fn greeter(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+pub fn default_greeter() -> String {
+    greeter(&mod1::world())
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_greeter() {
+        assert_eq!(super::greeter("Bob"), "Hello, Bob!");
+    }
+
+    #[test]
+    fn test_default_greeter() {
+        assert_eq!(super::default_greeter(), "Hello, world!");
+    }
+}
