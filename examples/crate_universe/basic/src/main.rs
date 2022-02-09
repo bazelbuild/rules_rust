9a680836f0d4872ff7707171311fc9b4e660abf7
Jeremiah Orem <jorem@etsy.com>
2022-02-09 14:28:55 -0800

    fmt example

--- examples/crate_universe/basic/src/main.rs
+++ examples/crate_universe/basic/src/main.rs
@@ -16,7 +16,6 @@ fn main() {
     assert_eq!(HASHMAP["Daniel"], "Wagner-Hall");
     println!("It worked!");
 
-
     let bag = ValueBag::capture_display(&42);
     let num = bag.to_u64().unwrap();
     println!("hello {}", num);
