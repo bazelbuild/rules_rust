2607c73d7b0af61aacc456d322be14b6b3477b2f
Gabriel Féron <g@leirbag.net>
2023-03-22 16:38:11 +0100

    Adjust msg

--- test/portable_link_flags_darwin/test.sh
+++ test/portable_link_flags_darwin/test.sh
@@ -2,11 +2,11 @@
 set -e
 
 if otool -L $1 | grep -q libtest; then
-    echo "built binary is dynamically linked against libtest!"
+    echo "error: rust_binary is dynamically linked against libtest!"
     exit 1
 fi
 
 if otool -L $1 | grep -q libstd; then
-    echo "built binary is dynamically linked against libstd!"
+    echo "error: rust_binary is dynamically linked against libstd!"
     exit 1
 fi
