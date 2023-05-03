f4adc923bd79c08cdc141d9fb03eea62b4eba5d0
Rosica Dejanovska <rosica@google.com>
2023-05-03 14:12:17 +0200

    Remove print line

--- test/no_std/main.rs
+++ test/no_std/main.rs
@@ -29,26 +29,8 @@ fn exit(status: i32) -> ! {
     }
 }
 
-fn write(fd: i32, buf: &[u8]) -> isize {
-    let r0;
-    unsafe {
-        asm!(
-            "syscall",
-            inlateout("rax") 1isize => r0,
-            in("rdi") fd,
-            in("rsi") buf.as_ptr(),
-            in("rdx") buf.len(),
-            lateout("rcx") _,
-            lateout("r11") _,
-            options(nostack, preserves_flags)
-        );
-    }
-    r0
-}
-
 #[no_mangle]
 unsafe fn start_main(_stack_top: *const u8) -> ! {
-    write(1, "Hello, world\n".as_bytes());
     exit(0);
 }
 
