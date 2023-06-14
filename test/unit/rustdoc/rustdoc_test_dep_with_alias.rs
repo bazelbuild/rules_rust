b71454bbfb797545cc6aa0320ef80f6c2e4400c5
k1nkreet <polyakovskiy.ilya@gmail.com>
2023-06-14 17:05:42 +0200

    add renamed dependency usage inside the doc-string to check rust_doc_test working with aliases

--- test/unit/rustdoc/rustdoc_test_dep_with_alias.rs
+++ test/unit/rustdoc/rustdoc_test_dep_with_alias.rs
@@ -1,5 +1,8 @@
+/// Here we are trying to use renamed dependency
+/// inside a doc string to check rust_doc_test
 /// ```
-/// let v = 42;
+/// use aliased_adder;
+/// let v = aliased_adder::inc(42);
 /// ```
 pub fn inc(n: u32) -> u32 {
     aliased_adder::inc(n)
