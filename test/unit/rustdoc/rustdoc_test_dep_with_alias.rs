c7777d65ed02b83617919d5caef18677689942d5
Ilya Polyakovskiy <ilya.polyakovskiy@tweag.io>
2023-06-14 17:21:34 +0200

    Update test/unit/rustdoc/rustdoc_test_dep_with_alias.rs
    Co-authored-by: UebelAndre <github@uebelandre.com>

--- test/unit/rustdoc/rustdoc_test_dep_with_alias.rs
+++ test/unit/rustdoc/rustdoc_test_dep_with_alias.rs
@@ -2,7 +2,7 @@
 /// inside a doc string to check rust_doc_test
 /// ```
 /// use aliased_adder;
-/// let v = aliased_adder::inc(42);
+///  assert_eq!(43, aliased_adder::inc(42));
 /// ```
 pub fn inc(n: u32) -> u32 {
     aliased_adder::inc(n)
