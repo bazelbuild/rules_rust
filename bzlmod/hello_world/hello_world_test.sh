763043ac0845999d517a7f2a57c7af49a40e53d6
Chuck Grindel <chuck.grindel@gmail.com>
2023-12-27 09:41:35 -0700

    Pass location of binary via args.

--- bzlmod/hello_world/hello_world_test.sh
+++ bzlmod/hello_world/hello_world_test.sh
@@ -1,29 +1,18 @@
 #!/usr/bin/env bash
 
-# --- begin runfiles.bash initialization v2 ---
-# Copy-pasted from the Bazel Bash runfiles library v2.
-set -o nounset -o pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
-# shellcheck disable=SC1090
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$0.runfiles/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -o errexit
-# --- end runfiles.bash initialization v2 ---
-
-# MARK - Locate Deps
-
-hello_world_location=hello_world_example/hello_world
-hello_world="$(rlocation "${hello_world_location}")" || \
-  (echo >&2 "Failed to locate ${hello_world_location}" && exit 1)
-
 # MARK - Functions
 
 fail() {
   echo >&2 "$@"
   exit 1
 }
+
+# MARK - Args
+
+if [[ "$#" -ne 1 ]]; then
+  fail "Usage: $0 /path/to/hello_world"
+fi
+hello_world="$1"
 
 # MARK - Test
 
