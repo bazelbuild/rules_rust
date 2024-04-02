5e0a723307e3d6ebd83fbeb461ea5fdf3fdf17b2
David Zbarsky <dzbarsky@gmail.com>
2024-04-02 15:34:11 -0400

    buildifier

--- test/bzlmod_repo_mapping/module_a/lib.rs
+++ test/bzlmod_repo_mapping/module_a/lib.rs
@@ -3,6 +3,6 @@ mod test {
     #[test]
     fn test_repo_remapping() {
         let data = lib_b::read_file_from_module_c();
-        assert_eq!(data, "module(name = \"module_c\")");
+        assert_eq!(data, "module(name = \"module_c\")\n");
     }
 }
