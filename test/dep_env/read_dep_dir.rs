092dbbf1d524e0393b0912f1d6906b9d466b844b
UebelAndre <github@uebelandre.com>
2024-09-18 12:16:33 -0700

    Better error message

--- test/dep_env/read_dep_dir.rs
+++ test/dep_env/read_dep_dir.rs
@@ -3,7 +3,7 @@ use std::{env::var, fs, io::Result};
 fn main() {
     let dep_dir = &var("DEP_DIR").expect("DEP_DIR should be set");
     let entries = fs::read_dir(dep_dir)
-        .expect("Failed to open DEP_DIR directory")
+        .unwrap_or_else(|e| panic!("Failed to open DEP_DIR directory: {}\n{:?}", dep_dir, e))
         .collect::<Result<Vec<_>>>()
         .expect("Failed to read DEP_DIR directory entries");
     let entries = entries
