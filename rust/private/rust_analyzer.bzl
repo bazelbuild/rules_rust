bazelbuild/rules_rust

commit 74f164b9dda7d910816a000542fd8749ee12307c

rust_analyzer: make all paths in rust-project.json absolute (#3033)
This is a step towards supporting automatic project discovery (#2755).

Relative paths are resolved against the location of rust-project.json,
but in auto-discovery this is the location of the BUILD file being
discovered, not the workspace root.

We already use absolute paths for generated files, so rust-project.json
is not workspace-independent today.

The alternatives seem more complex for little gain:

- Only use absolute paths for auto-discovery mode, relative otherwise.
- Use ../../ to express all workspace paths relative to the BUILD.

See
https://github.com/bazelbuild/rules_rust/issues/2755#issuecomment-250696107

rust/private/rust_analyzer.bzl
@@ -187,6 +187,9 @@ rust_analyzer_aspect = aspect(
     doc = "Annotates rust rules with RustAnalyzerInfo later used to build a rust-project.json",
 )
 
+# Paths in the generated JSON file begin with one of these placeholders.
+# The gen_rust_project driver will replace them with absolute paths.
+_WORKSPACE_TEMPLATE = "__WORKSPACE__/"
 _EXEC_ROOT_TEMPLATE = "__EXEC_ROOT__/"
 _OUTPUT_BASE_TEMPLATE = "__OUTPUT_BASE__/"
 
@@ -222,7 +225,7 @@ def _create_single_crate(ctx, attrs, info):
     # TODO: Some folks may want to override this for vendored dependencies.
     is_external = info.crate.root.path.startswith("external/")
     is_generated = not info.crate.root.is_source
-    path_prefix = _EXEC_ROOT_TEMPLATE if is_external or is_generated else ""
+    path_prefix = _EXEC_ROOT_TEMPLATE if is_external or is_generated else _WORKSPACE_TEMPLATE
     crate["is_workspace_member"] = not is_external
     crate["root_module"] = path_prefix + info.crate.root.path
     crate["source"] = {"exclude_dirs": [], "include_dirs": []}
@@ -231,7 +234,7 @@ def _create_single_crate(ctx, attrs, info):
         srcs = getattr(ctx.rule.files, "srcs", [])
         src_map = {src.short_path: src for src in srcs if src.is_source}
         if info.crate.root.short_path in src_map:
-            crate["root_module"] = src_map[info.crate.root.short_path].path
+            crate["root_module"] = _WORKSPACE_TEMPLATE + src_map[info.crate.root.short_path].path
             crate["source"]["include_dirs"].append(path_prefix + info.crate.root.dirname)
 
     if info.build_info != None and info.build_info.out_dir != None:
@@ -263,7 +266,8 @@ def _create_single_crate(ctx, attrs, info):
     crate["deps"] = [_crate_id(dep.crate) for dep in info.deps if _crate_id(dep.crate) != crate_id]
     crate["aliases"] = {_crate_id(alias_target.crate): alias_name for alias_target, alias_name in info.aliases.items()}
     crate["cfg"] = info.cfgs
-    crate["target"] = find_toolchain(ctx).target_flag_value
+    toolchain = find_toolchain(ctx)
+    crate["target"] = (_EXEC_ROOT_TEMPLATE + toolchain.target_json.path) if toolchain.target_json else toolchain.target_flag_value
     if info.proc_macro_dylib_path != None:
         crate["proc_macro_dylib_path"] = _EXEC_ROOT_TEMPLATE + info.proc_macro_dylib_path
     return crate
@@ -315,6 +319,8 @@ def _rust_analyzer_detect_sysroot_impl(ctx):
     sysroot_src = rustc_srcs.label.package + "/library"
     if rustc_srcs.label.workspace_root:
         sysroot_src = _OUTPUT_BASE_TEMPLATE + rustc_srcs.label.workspace_root + "/" + sysroot_src
+    else:
+        sysroot_src = _WORKSPACE_TEMPLATE + sysroot_src
 
     rustc = rust_analyzer_toolchain.rustc
     sysroot_dir, _, bin_dir = rustc.dirname.rpartition("/")
@@ -323,10 +329,7 @@ def _rust_analyzer_detect_sysroot_impl(ctx):
             rustc.path,
         ))
 
-    sysroot = "{}/{}".format(
-        _OUTPUT_BASE_TEMPLATE,
-        sysroot_dir,
-    )
+    sysroot = _OUTPUT_BASE_TEMPLATE + sysroot_dir
 
     toolchain_info = {
         "sysroot": sysroot,

test/rust_analyzer/generated_srcs_test/rust_project_json_test.rs
@@ -1,43 +1,57 @@
 #[cfg(test)]
 mod tests {
+    use serde::Deserialize;
     use std::env;
     use std::path::PathBuf;
 
-    #[test]
-    fn test_deps_of_crate_and_its_test_are_merged() {
-        let rust_project_path = PathBuf::from(env::var("RUST_PROJECT_JSON").unwrap());
+    #[derive(Deserialize)]
+    struct Project {
+        sysroot_src: String,
+        crates: Vec<Crate>,
+    }
 
+    #[derive(Deserialize)]
+    struct Crate {
+        display_name: String,
+        root_module: String,
+        source: Option<Source>,
+    }
+
+    #[derive(Deserialize)]
+    struct Source {
+        include_dirs: Vec<String>,
+    }
+
+    #[test]
+    fn test_generated_srcs() {
+        let rust_project_path = PathBuf::from(env::var("RUST_PROJECT_JSON").unwrap());
         let content = std::fs::read_to_string(&rust_project_path)
             .unwrap_or_else(|_| panic!("couldn't open {:?}", &rust_project_path));
-
-        let output_base = content
-            .lines()
-            .find(|text| text.trim_start().starts_with("\"sysroot_src\":"))
-            .map(|text| {
-                let mut split = text.splitn(2, "\"sysroot_src\": ");
-                let mut with_hash = split.nth(1).unwrap().trim().splitn(2, "/external/");
-                let mut output = with_hash.next().unwrap().rsplitn(2, '/');
-                output.nth(1).unwrap()
-            })
-            .expect("Failed to find sysroot entry.");
-
-        let expected = r#"{
-      "display_name": "generated_srcs",
-      "root_module": "lib.rs",
-      "edition": "2021",
-      "deps": [],
-      "is_workspace_member": true,
-      "source": {
-        "include_dirs": [
-          "#
-        .to_owned()
-            + output_base;
-
         println!("{}", content);
-        assert!(
-            content.contains(&expected),
-            "expected rust-project.json to contain the following block:\n{}",
-            expected
-        );
+        let project: Project =
+            serde_json::from_str(&content).expect("Failed to deserialize project JSON");
+
+        // /tmp/_bazel/12345678/external/tools/rustlib/library => /tmp/_bazel
+        let output_base = project
+            .sysroot_src
+            .rsplitn(2, "/external/")
+            .last()
+            .unwrap()
+            .rsplitn(2, '/')
+            .last()
+            .unwrap();
+        println!("output_base: {output_base}");
+
+        let gen = project
+            .crates
+            .iter()
+            .find(|c| &c.display_name == "generated_srcs")
+            .unwrap();
+        assert!(gen.root_module.starts_with("/"));
+        assert!(gen.root_module.ends_with("/lib.rs"));
+
+        let include_dirs = &gen.source.as_ref().unwrap().include_dirs;
+        assert!(include_dirs.len() == 1);
+        assert!(include_dirs[0].starts_with(output_base));
     }
 }

test/rust_analyzer/merging_crates_test/rust_project_json_test.rs
@@ -1,33 +1,41 @@
 #[cfg(test)]
 mod tests {
+    use serde::Deserialize;
     use std::env;
     use std::path::PathBuf;
+
+    #[derive(Deserialize)]
+    struct Project {
+        crates: Vec<Crate>,
+    }
+
+    #[derive(Deserialize)]
+    struct Crate {
+        display_name: String,
+        deps: Vec<Dep>,
+    }
+
+    #[derive(Deserialize)]
+    struct Dep {
+        name: String,
+    }
 
     #[test]
     fn test_deps_of_crate_and_its_test_are_merged() {
         let rust_project_path = PathBuf::from(env::var("RUST_PROJECT_JSON").unwrap());
-
         let content = std::fs::read_to_string(&rust_project_path)
             .unwrap_or_else(|_| panic!("couldn't open {:?}", &rust_project_path));
-
-        let expected = r#"{
-      "display_name": "mylib",
-      "root_module": "mylib.rs",
-      "edition": "2018",
-      "deps": [
-        {
-          "crate": 0,
-          "name": "extra_test_dep"
-        },
-        {
-          "crate": 1,
-          "name": "lib_dep"
-        }
-      ],"#;
-
         println!("{}", content);
-        assert!(
-            content.contains(expected),
-            "expected rust-project.json to contain both lib_dep and extra_test_dep in deps of mylib.rs.");
+        let project: Project =
+            serde_json::from_str(&content).expect("Failed to deserialize project JSON");
+
+        let lib = project
+            .crates
+            .iter()
+            .find(|c| &c.display_name == "mylib")
+            .unwrap();
+        let mut deps = lib.deps.iter().map(|d| &d.name).collect::<Vec<_>>();
+        deps.sort();
+        assert!(deps == vec!["extra_test_dep", "lib_dep"]);
     }
 }

test/rust_analyzer/static_and_shared_lib_test/rust_project_json_test.rs
@@ -1,31 +1,41 @@
 #[cfg(test)]
 mod tests {
+    use serde::Deserialize;
     use std::env;
     use std::path::PathBuf;
 
-    #[test]
-    fn test_deps_of_crate_and_its_test_are_merged() {
-        let rust_project_path = PathBuf::from(env::var("RUST_PROJECT_JSON").unwrap());
+    #[derive(Deserialize)]
+    struct Project {
+        crates: Vec<Crate>,
+    }
 
+    #[derive(Deserialize)]
+    struct Crate {
+        display_name: String,
+        root_module: String,
+    }
+
+    #[test]
+    fn test_static_and_shared_lib() {
+        let rust_project_path = PathBuf::from(env::var("RUST_PROJECT_JSON").unwrap());
         let content = std::fs::read_to_string(&rust_project_path)
             .unwrap_or_else(|_| panic!("couldn't open {:?}", &rust_project_path));
-
         println!("{}", content);
+        let project: Project =
+            serde_json::from_str(&content).expect("Failed to deserialize project JSON");
 
-        let expected_cdylib = r#"{
-      "display_name": "greeter_cdylib",
-      "root_module": "shared_lib.rs","#;
-        assert!(
-            content.contains(expected_cdylib),
-            "expected rust-project.json to contain a rust_shared_library target."
-        );
+        let cdylib = project
+            .crates
+            .iter()
+            .find(|c| &c.display_name == "greeter_cdylib")
+            .unwrap();
+        assert!(cdylib.root_module.ends_with("/shared_lib.rs"));
 
-        let expected_staticlib = r#"{
-      "display_name": "greeter_staticlib",
-      "root_module": "static_lib.rs","#;
-        assert!(
-            content.contains(expected_staticlib),
-            "expected rust-project.json to contain a rust_static_library target."
-        );
+        let staticlib = project
+            .crates
+            .iter()
+            .find(|c| &c.display_name == "greeter_staticlib")
+            .unwrap();
+        assert!(staticlib.root_module.ends_with("/static_lib.rs"));
     }
 }

test/rust_analyzer/generated_srcs_test/BUILD.bazel
@@ -34,4 +34,8 @@ rust_test(
     # contexts outside of `//test/rust_analyzer:rust_analyzer_test`. Run
     # that target to execute this test.
     tags = ["manual"],
+    deps = [
+        "@rules_rust//test/3rdparty/crates:serde",
+        "@rules_rust//test/3rdparty/crates:serde_json",
+    ],
 )

test/rust_analyzer/merging_crates_test/BUILD.bazel
@@ -36,4 +36,8 @@ rust_test(
     # contexts outside of `//test/rust_analyzer:rust_analyzer_test`. Run
     # that target to execute this test.
     tags = ["manual"],
+    deps = [
+        "@rules_rust//test/3rdparty/crates:serde",
+        "@rules_rust//test/3rdparty/crates:serde_json",
+    ],
 )

test/rust_analyzer/static_and_shared_lib_test/BUILD.bazel
@@ -35,4 +35,8 @@ rust_test(
     # contexts outside of `//test/rust_analyzer:rust_analyzer_test`. Run
     # that target to execute this test.
     tags = ["manual"],
+    deps = [
+        "@rules_rust//test/3rdparty/crates:serde",
+        "@rules_rust//test/3rdparty/crates:serde_json",
+    ],
 )

tools/rust_analyzer/lib.rs
@@ -74,6 +74,7 @@ pub fn write_rust_project(
 
     rust_project::write_rust_project(
         rust_project_path.as_ref(),
+        workspace.as_ref(),
         execution_root.as_ref(),
         output_base.as_ref(),
         &rust_project,

commit 965c2c8c192039aef3fd157013cc8362fd9c1f4c

Mitigate & document problems with mixed generated sources
- deterministically prefer rust_library's crate spec over rust_test's.
  This means root_module gets remapped to a workspace path more often,
  and fixes flakiness of generated_srcs_test.
- document the reasons and tradeoffs for this remapping.
- stop adding `include_dirs` when remapping, it doesn't do anything to
  help with this problem, and is confusing.
- fix test to strictly assert the path, which I broke in
  74f164b9dda7d910816a000542fd8749ee12307c

Partially fixes https://github.com/bazelbuild/rules_rust/issues/3126

rust/private/rust_analyzer.bzl
@@ -230,12 +230,24 @@ def _create_single_crate(ctx, attrs, info):
     crate["root_module"] = path_prefix + info.crate.root.path
     crate["source"] = {"exclude_dirs": [], "include_dirs": []}
 
+    # If some sources are generated and others are not, paths are complicated.
+    #
+    # bazel splits the files across bazel-out/package/* and workspace/package/*,
+    # but rustc requires the sources to all be in one directory.
+    # To address this, rules_rust adds symlinks: bazel-out/package/* => workspace/package/*
+    #
+    # If we specify one of the symlinks as our root_module, then rust-analyzer can index the crate,
+    # but won't recognize files in the workspace as being part of it, so features won't work.
+    # If we specify the path within the workspace, it won't manage to resolve the generated modules.
+    #
+    # There's no perfect solution here, for now we choose to try to find a workspace path.
+    # https://github.com/bazelbuild/rules_rust/issues/3126
     if is_generated:
-        srcs = getattr(ctx.rule.files, "srcs", [])
-        src_map = {src.short_path: src for src in srcs if src.is_source}
-        if info.crate.root.short_path in src_map:
-            crate["root_module"] = _WORKSPACE_TEMPLATE + src_map[info.crate.root.short_path].path
-            crate["source"]["include_dirs"].append(path_prefix + info.crate.root.dirname)
+        # 'root' is a transformed source, likely a symlink.
+        # 'srcs' are what the user originally specified, likely workspace paths.
+        for src in getattr(ctx.rule.files, "srcs", []):
+          if src.is_source and info.crate.root.short_path == src.short_path:
+            crate["root_module"] = _WORKSPACE_TEMPLATE + src.path
 
     if info.build_info != None and info.build_info.out_dir != None:
         out_dir_path = info.build_info.out_dir.path

tools/rust_analyzer/aquery.rs
@@ -176,19 +176,22 @@ fn consolidate_crate_specs(crate_specs: Vec<CrateSpec>) -> anyhow::Result<BTreeS
     for mut spec in crate_specs.into_iter() {
         log::debug!("{:?}", spec);
         if let Some(existing) = consolidated_specs.get_mut(&spec.crate_id) {
+            // Prefer rlib crates over bin crates (typically tests):
+            // - the display name that matches the crate name (foo vs foo_test)
+            //   Rust Analyzer seems to use display_name for matching crate entries
+            //   in rust-project.json against symbols in source files.
+            //   https://github.com/bazelbuild/rules_rust/issues/1032
+            // - test crates may report a genfiles path as the root_module, rather
+            //   than the original source file in the workspace.
+            //   This causes rust-analyzer to not recognize the workspace file.
+            //   https://github.com/bazelbuild/rules_rust/issues/3126
+            if spec.crate_type == "rlib" {
+                std::mem::swap(&mut spec, existing);
+            }
             existing.deps.extend(spec.deps);
 
             spec.cfg.retain(|cfg| !existing.cfg.contains(cfg));
             existing.cfg.extend(spec.cfg);
-
-            // display_name should match the library's crate name because Rust Analyzer
-            // seems to use display_name for matching crate entries in rust-project.json
-            // against symbols in source files. For more details, see
-            // https://github.com/bazelbuild/rules_rust/issues/1032
-            if spec.crate_type == "rlib" {
-                existing.display_name = spec.display_name;
-                existing.crate_type = "rlib".into();
-            }
 
             // For proc-macro crates that exist within the workspace, there will be a
             // generated crate-spec in both the fastbuild and opt-exec configuration.

test/rust_analyzer/generated_srcs_test/rust_project_json_test.rs
@@ -6,7 +6,6 @@ mod tests {
 
     #[derive(Deserialize)]
     struct Project {
-        sysroot_src: String,
         crates: Vec<Crate>,
     }
 
@@ -14,12 +13,6 @@ mod tests {
     struct Crate {
         display_name: String,
         root_module: String,
-        source: Option<Source>,
-    }
-
-    #[derive(Deserialize)]
-    struct Source {
-        include_dirs: Vec<String>,
     }
 
     #[test]
@@ -31,27 +24,20 @@ mod tests {
         let project: Project =
             serde_json::from_str(&content).expect("Failed to deserialize project JSON");
 
-        // /tmp/_bazel/12345678/external/tools/rustlib/library => /tmp/_bazel
-        let output_base = project
-            .sysroot_src
-            .rsplitn(2, "/external/")
-            .last()
-            .unwrap()
-            .rsplitn(2, '/')
-            .last()
-            .unwrap();
-        println!("output_base: {output_base}");
-
         let gen = project
             .crates
             .iter()
             .find(|c| &c.display_name == "generated_srcs")
             .unwrap();
-        assert!(gen.root_module.starts_with("/"));
-        assert!(gen.root_module.ends_with("/lib.rs"));
 
-        let include_dirs = &gen.source.as_ref().unwrap().include_dirs;
-        assert!(include_dirs.len() == 1);
-        assert!(include_dirs[0].starts_with(output_base));
+        // This target has mixed generated+plain sources, so rules_rust provides a
+        // directory where the root module is a symlink.
+        // However in the crate spec, we want the root_module to be a workspace path
+        // when possible.
+        let workspace_path = PathBuf::from(env::var("WORKSPACE").unwrap());
+        assert_eq!(
+            gen.root_module,
+            workspace_path.join("lib.rs").to_string_lossy()
+        );
     }
 }

test/rust_analyzer/rust_analyzer_test_runner.sh
@@ -101,7 +101,7 @@ function rust_analyzer_test() {
     echo "Building..."
     bazel build //...
     echo "Testing..."
-    bazel test //...
+    bazel test --test_env=WORKSPACE="${workspace}" //...
     echo "Building with Aspects..."
     bazel build //... --config=strict
     popd &>/dev/null