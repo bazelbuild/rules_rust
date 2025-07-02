3074ed38e0df3ca0d1255986390f7d8db277510a
Daniel Wagner-Hall <dawagner@gmail.com>
2025-07-02 22:54:27 +0100

    Fix panic line numbers
    Without this patch, line numbers are missing for first-party code in
    panic traces.
    
    This reverts part of #1563 but keeps the test showing we have a relative
    path not an absolute one, and adds an integration test showing that line
    numbers are correctly reported in panic traces.
    
    As far as I know, path remapping has been improved in rustc over the
    last few years, so maybe the behaviour reported in that patch has been
    fixed.

--- test/unit/remap_path_prefix/test.rs
+++ test/unit/remap_path_prefix/test.rs
@@ -2,6 +2,6 @@
 fn test_dep_file_name() {
     assert_eq!(
         dep::get_file_name::<()>(),
-        "test/unit/remap_path_prefix/dep.rs"
+        "./test/unit/remap_path_prefix/dep.rs"
     );
 }
