7501b09bba61b8c32e25ae743124dabb1a51acf2
Daniel Wagner-Hall <dawagner@gmail.com>
2025-07-02 23:42:31 +0100

    Cross-platform expectation

--- test/unit/remap_path_prefix/test.rs
+++ test/unit/remap_path_prefix/test.rs
@@ -1,7 +1,13 @@
 #[test]
 fn test_dep_file_name() {
+    let mut expected = std::path::PathBuf::from(".");
+    expected.push("test");
+    expected.push("unit");
+    expected.push("remap_path_prefix");
+    expected.push("dep.rs");
+    let expected_str = expected.to_str().unwrap();
     assert_eq!(
         dep::get_file_name::<()>(),
-        "./test/unit/remap_path_prefix/dep.rs"
+        expected_str
     );
 }
