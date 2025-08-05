1fada2e7678eb632289893bd8be2fee2654a0819
Cameron Martin <cameronm@graphcore.ai>
2025-08-05 20:51:41 +0100

    Try renaming archive in case the issue is name collisions

--- test/linking_out_dir/build.rs
+++ test/linking_out_dir/build.rs
@@ -1,15 +1,17 @@
 use std::env;
 use std::path::PathBuf;
 
+const ARCHIVE_NAME: &'static str = "test_linking_out_dir_foo";
+
 fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
     let src = env::var_os("ARCHIVE_PATH").unwrap();
 
-    let dst = out_dir.join("libfoo.a");
+    let dst = out_dir.join(format!("lib{ARCHIVE_NAME}.a"));
 
     std::fs::copy(&src, &dst).unwrap();
 
-    println!("cargo:rustc-link-lib=foo");
+    println!("cargo:rustc-link-lib={ARCHIVE_NAME}");
     println!("cargo:rustc-link-search=native={}", out_dir.display());
 }
