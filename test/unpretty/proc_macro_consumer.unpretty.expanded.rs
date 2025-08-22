9c619a87b6156ef2cda9f304666226789d887f3f
UebelAndre <github@uebelandre.com>
2025-08-22 09:49:45 -0700

    Updated unpretty tests

--- test/unpretty/proc_macro_consumer.unpretty.expanded.rs
+++ test/unpretty/proc_macro_consumer.unpretty.expanded.rs
@@ -1,8 +1,8 @@
 #![feature(prelude_import)]
-#[prelude_import]
-use std::prelude::rust_2021::*;
 #[macro_use]
 extern crate std;
+#[prelude_import]
+use std::prelude::rust_2021::*;
 use proc_macro::make_answer;
 
 fn answer() -> u32 { 42 }
