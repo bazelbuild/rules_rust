e19bd9cdf3789eccbed21731625e8bb98df70a27
Andy Scott <andy.g.scott@gmail.com>
2025-11-14 21:42:53 -0800

    updates for linters

--- extensions/pyo3/test/module_prefix/module_prefix_import_test.py
+++ extensions/pyo3/test/module_prefix/module_prefix_import_test.py
@@ -2,11 +2,15 @@
 
 import unittest
 
-import foo.bar
+import foo.bar  # type: ignore
 
 
 class ModulePrefixImportTest(unittest.TestCase):
+    """Test Class."""
+
     def test_import_and_call(self) -> None:
+        """Test that a pyo3 extension can be imported via a module prefix."""
+
         result = foo.bar.thing()
         self.assertEqual("hello from rust", result)
 
