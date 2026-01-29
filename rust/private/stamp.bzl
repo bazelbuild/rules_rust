bazelbuild/rules_rust

commit 9586468d1eaa8d22f527966cb5f43a4870463649

Switch stamping detection to ctx.configuration.stamp_binaries() (#3816)
Currently, rules_rust uses a `config_setting` that matches `--stamp=1`
to detect whether the `--stamp` option is enabled in the current
configuration. This PR removes the `config_setting` in favor of calling
`ctx.configuration.stamp_binaries()`, which is restricted, but available
to rules rust because it is on the restricted starlark API allowlist:
https://cs.opensource.google/bazel/bazel/+/master:src/main/java/com/google/devtools/build/lib/packages/BuiltinRestriction.java;l=73;drc=135a03ba5dbad5a1a26ef3bde9815ea244a093d7.

The motivation for making this change is to improve Google-internal
detection of stamped actions for cross-user caching eligibility. The
`ctx.configuration.stamp_binaries()` call is limited to the case of
`stamp = -1`, whereas the `config_setting` dependency was unconditional.
Notably, for `rust_library` rules with the default attribute of `stamp =
0`, we are now able to certify that the rule is not affected by the
`--stamp` flag value.

Google-internal bug number: 419546090

---------

Co-authored-by: Krasimir Georgiev <krasimir@google.com>

rust/private/stamp.bzl
@@ -1,63 +1,10 @@
-"""A small utility module dedicated to detecting whether or not the `--stamp` flag is enabled
+"""A small utility module dedicated to detecting whether or not the `--stamp` flag is enabled"""
 
-This module can be removed likely after the following PRs ar addressed:
-- https://github.com/bazelbuild/bazel/issues/11164
-"""
-
-load("//rust/private:utils.bzl", "dedent")
-
-StampSettingInfo = provider(
-    doc = "Information about the `--stamp` command line flag",
-    fields = {
-        "value": "bool: Whether or not the `--stamp` flag was enabled",
-    },
-)
-
-def _stamp_build_setting_impl(ctx):
-    return StampSettingInfo(value = ctx.attr.value)
-
-_stamp_build_setting = rule(
-    doc = dedent("""\
-        Whether to encode build information into the binary. Possible values:
-
-        - stamp = 1: Always stamp the build information into the binary, even in [--nostamp][stamp] builds. \
-        This setting should be avoided, since it potentially kills remote caching for the binary and \
-        any downstream actions that depend on it.
-        - stamp = 0: Always replace build information by constant values. This gives good build result caching.
-        - stamp = -1: Embedding of build information is controlled by the [--[no]stamp][stamp] flag.
-
-        Stamped binaries are not rebuilt unless their dependencies change.
-        [stamp]: https://docs.bazel.build/versions/main/user-manual.html#flag--stamp
-    """),
-    implementation = _stamp_build_setting_impl,
-    attrs = {
-        "value": attr.bool(
-            doc = "The default value of the stamp build flag",
-            mandatory = True,
-        ),
-    },
-)
-
-def stamp_build_setting(name, visibility = ["//visibility:public"]):
-    native.config_setting(
-        name = "stamp_detect",
-        values = {"stamp": "1"},
-        visibility = visibility,
-    )
-
-    _stamp_build_setting(
-        name = name,
-        value = select({
-            ":stamp_detect": True,
-            "//conditions:default": False,
-        }),
-        visibility = visibility,
-    )
-
-def is_stamping_enabled(attr):
+def is_stamping_enabled(ctx, attr):
     """Determine whether or not build stamping is enabled
 
     Args:
+        ctx (ctx): The rule's context object
         attr (struct): A rule's struct of attributes (`ctx.attr`)
 
     Returns:
@@ -69,7 +16,6 @@ def is_stamping_enabled(attr):
     elif stamp_num == 0:
         return False
     elif stamp_num == -1:
-        stamp_flag = getattr(attr, "_stamp_flag", None)
-        return stamp_flag[StampSettingInfo].value if stamp_flag else False
+        return ctx.configuration.stamp_binaries()
     else:
         fail("Unexpected `stamp` value: {}".format(stamp_num))

rust/private/BUILD.bazel
@@ -1,7 +1,6 @@
 load("@bazel_skylib//:bzl_library.bzl", "bzl_library")
 load("//rust/private:rust_analyzer.bzl", "rust_analyzer_detect_sysroot")
 load("//rust/private:rustc.bzl", "is_proc_macro_dep", "is_proc_macro_dep_enabled")
-load("//rust/private:stamp.bzl", "stamp_build_setting")
 
 # Exported for docs
 exports_files(["providers.bzl"])
@@ -32,8 +31,6 @@ bzl_library(
         "@bazel_skylib//rules:common_settings",
     ],
 )
-
-stamp_build_setting(name = "stamp")
 
 # This setting may be used to identify dependencies of proc-macro-s.
 # This feature is only enabled if `is_proc_macro_dep_enabled` is true.

rust/private/rust.bzl
@@ -826,10 +826,6 @@ _common_attrs = {
         doc = "Enable collection of cfg flags with results stored in CrateInfo.cfgs.",
         default = Label("//rust/settings:collect_cfgs"),
     ),
-    "_stamp_flag": attr.label(
-        doc = "A setting used to determine whether or not the `--stamp` flag is enabled",
-        default = Label("//rust/private:stamp"),
-    ),
 } | RUSTC_ATTRS | RUSTC_ALLOCATOR_LIBRARIES_ATTRS
 
 _coverage_attrs = {

rust/private/rustc.bzl
@@ -1349,7 +1349,7 @@ def rustc_compile_action(
         linkstamps = depset([])
 
     # Determine if the build is currently running with --stamp
-    stamp = is_stamping_enabled(attr)
+    stamp = is_stamping_enabled(ctx, attr)
 
     # Add flags for any 'rustc' lints that are specified.
     #

commit f198ddee7f49ac351d27204b1488df0af2512fac

Fix stamping for rules that don't have a stamp attribute (#3829)
Rules that don't have a `stamp` attribute, for example
`rust_cc_proto_library_aspect`, should not be treated as stamped. This
was broken by https://github.com/bazelbuild/rules_rust/pull/3816 - prior
to that, we were returning false because they also don't have a
`_stamp_flag ` attribute.

By returning true for `rust_cc_proto_library_aspect`, we're
unnecessarily including the volatile stamp files as action inputs, which
harms build caching.

---------

Co-authored-by: Krasimir Georgiev <krasimir@google.com>

rust/private/stamp.bzl
@@ -10,7 +10,7 @@ def is_stamping_enabled(ctx, attr):
     Returns:
         bool: The stamp value
     """
-    stamp_num = getattr(attr, "stamp", -1)
+    stamp_num = getattr(attr, "stamp", 0)
     if stamp_num == 1:
         return True
     elif stamp_num == 0: